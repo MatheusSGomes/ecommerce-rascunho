4962e941f754d9153d9f8a16577f8cbee66d2ba3
Matheus Gomes <math.s.gomes@gmail.com>
2024-09-03 21:37:39 -0300

    add usuarios controller and method get usuario

---
+++ eCommerce.API/Controllers/UsuariosController.cs
@@ -0,0 +1,25 @@
+using eCommerce.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly IUsuarioRepository _repository;
+
+        public UsuariosController(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var listaUsuarios = _repository.Get();
+            return Ok(listaUsuarios);
+        }
+    }
+}
