62760446d2fb1381187ab2f725bc1675e45b7f22
Matheus Gomes <math.s.gomes@gmail.com>
2024-09-04 18:09:01 -0300

    add packages ef core

--- eCommerce.Models/Usuario.cs
+++ eCommerce.Models/Usuario.cs
@@ -13,5 +13,5 @@ public class Usuario
     public DateTimeOffset DataCadastro { get; set; }
     public Contato? Contato { get; set; }
     public ICollection<EnderecoEntrega>? EnderecosEntrega { get; set; }
-    public ICollection<Departamento> Departamentos { get; set; }
+    public ICollection<Departamento>? Departamentos { get; set; }
 }
