2f3f589aa84f99e20bc238c07e197c1e5fea7d34
Matheus Gomes <math.s.gomes@gmail.com>
2024-09-04 22:38:54 -0300

    add change properties of models

--- eCommerce.Models/Departamento.cs
+++ eCommerce.Models/Departamento.cs
@@ -3,6 +3,6 @@ namespace eCommerce.Models;
 public class Departamento
 {
     public int Id { get; set; }
-    public string? Nome { get; set; }
+    public string Nome { get; set; } = null!;
     public ICollection<Usuario>? Usuarios { get; set; }
 }
