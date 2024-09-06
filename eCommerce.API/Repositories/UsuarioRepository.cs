e26aa6f7580db39ee96034d96c9f117f0351b6b4
Matheus Gomes <math.s.gomes@gmail.com>
2024-09-05 22:41:24 -0300

    add db context to usuario repository

--- eCommerce.API/Repositories/UsuarioRepository.cs
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -1,34 +1,43 @@
+using eCommerce.API.Database;
 using eCommerce.Models;
 
 namespace eCommerce.API.Repositories;
 
 public class UsuarioRepository : IUsuarioRepository
 {
-    public static List<Usuario> _db = new List<Usuario>();
+    private readonly eCommerceContext _db;
+
+    public UsuarioRepository(eCommerceContext db)
+    {
+        _db = db;
+    }
 
     public List<Usuario> Get()
     {
-        return _db;
+        return _db.Usuarios.OrderBy(a => a.Id).ToList();
     }
 
     public Usuario Get(int id)
     {
-        return _db.Find(x => x.Id == id)!;
+        return _db.Usuarios.Find(id)!;
     }
 
     public void Add(Usuario usuario)
     {
         _db.Add(usuario);
+        _db.SaveChanges();
     }
 
     public void Update(Usuario usuario)
     {
-        _db.Remove(Get(usuario.Id));
-        _db.Add(usuario);
+        _db.Update(usuario);
+        _db.SaveChanges();
     }
 
     public void Delete(int id)
     {
-        _db.Remove(Get(id));
+        var usuario = Get(id);
+        _db.Remove(usuario);
+        _db.SaveChanges();
     }
 }
