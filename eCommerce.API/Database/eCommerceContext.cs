539bec33afe2ae50909d414df0bd6cd6f1f18a3d
Matheus Gomes <math.s.gomes@gmail.com>
2024-09-07 14:59:15 -0300

    add seeds departamentos

--- eCommerce.API/Database/eCommerceContext.cs
+++ eCommerce.API/Database/eCommerceContext.cs
@@ -12,6 +12,19 @@ public class eCommerceContext : DbContext
     public eCommerceContext(DbContextOptions<eCommerceContext> options) : base(options)
     { }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Departamento>()
+            .HasData(
+                new Departamento { Id = 1, Nome = "Moda" },
+                new Departamento { Id = 2, Nome = "Informática"},
+                new Departamento { Id = 3, Nome = "Eletrodomésticos"},
+                new Departamento { Id = 4, Nome = "Eletroportáteis"},
+                new Departamento { Id = 5, Nome = "Mercado"},
+                new Departamento { Id = 6, Nome = "Beleza"},
+                new Departamento { Id = 7, Nome = "Móveis"});
+    }
+
     #region Conexão com distinção de ambientes de execução 
     // Pesquisar strings de conexão: https://www.connectionstrings.com/sql-server/
     // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
