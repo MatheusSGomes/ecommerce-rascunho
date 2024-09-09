2130f215c1df28b997a0616f3bc0e4558b7ef8e0
Matheus Gomes <math.s.gomes@gmail.com>
2024-09-09 11:52:44 -0300

    add query to many to many

--- eCommerce.Office/Program.cs
+++ eCommerce.Office/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using eCommerce.Office;
+using Microsoft.EntityFrameworkCore;
 
-Console.WriteLine("Hello, World!");
+var db = new eCommerceOfficeContext();
+
+#region Many-To-Many > 2x One-To-Many
+// setor > colaboradorSetor > colaborador
+
+var resultado = db.Setores!
+    .Include(setor => setor.ColaboradorSetores)
+    .ThenInclude(colaboradorSetor => colaboradorSetor.Colaborador);
+
+foreach (var setor in resultado)
+{
+    Console.WriteLine(setor.Nome);
+
+    foreach (var colaboradorSetor in setor.ColaboradorSetores)
+    {
+        Console.WriteLine(" - " + colaboradorSetor.Colaborador.Nome);
+    }
+}
+#endregion
