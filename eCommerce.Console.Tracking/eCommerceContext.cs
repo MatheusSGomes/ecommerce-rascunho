7a58c59e341953e13d2eb51b9390e1c903281c35
Matheus Gomes <math.s.gomes@gmail.com>
2024-09-10 17:40:27 -0300

    add project to make tests with tracking

---
+++ eCommerce.Console.Tracking/eCommerceContext.cs
@@ -0,0 +1,17 @@
+using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.API.Database;
+
+public class eCommerceContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost,1433;Database=ecommerce;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False;");
+    }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Contato> Contatos { get; set; }
+    public DbSet<EnderecoEntrega> EnderecosEntrega { get; set; }
+    public DbSet<Departamento> Departamentos { get; set; }
+}
