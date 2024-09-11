7507b2f65b5fde863c8230ef9c4327dc45683777
Matheus Gomes <math.s.gomes@gmail.com>
2024-09-10 21:32:37 -0300

    add logs and no tracking to context

--- eCommerce.Console.Tracking/eCommerceContext.cs
+++ eCommerce.Console.Tracking/eCommerceContext.cs
@@ -7,7 +7,11 @@ public class eCommerceContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=localhost,1433;Database=ecommerce;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False;");
+        optionsBuilder
+            .UseSqlServer("Server=localhost,1433;Database=ecommerce;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False;")
+            .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
     }
 
     public DbSet<Usuario> Usuarios { get; set; }
