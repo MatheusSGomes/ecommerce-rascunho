9b6fde1490e12fc6f53811113ca44123d381542c
Matheus Gomes <math.s.gomes@gmail.com>
2024-09-12 21:22:02 -0300

    add project console extras

---
+++ eCommerce.Console.Extra/eCommerceContext.cs
@@ -0,0 +1,20 @@
+using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.API.Database;
+
+public class eCommerceContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer("Server=localhost,1433;Database=ecommerce;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False;")
+            /*.LogTo(System.Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+            .EnableSensitiveDataLogging()*/;
+    }
+
+    public DbSet<Usuario>? Usuarios { get; set; }
+    public DbSet<Contato>? Contatos { get; set; }
+    public DbSet<EnderecoEntrega>? EnderecosEntrega { get; set; }
+    public DbSet<Departamento>? Departamentos { get; set; }
+}
