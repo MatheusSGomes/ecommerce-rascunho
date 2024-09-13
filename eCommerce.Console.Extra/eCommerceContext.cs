3a9f76009c0593fe936aac1b3d31a6113ca892da
Matheus Gomes <math.s.gomes@gmail.com>
2024-09-13 11:21:04 -0300

    add temporal tables

--- eCommerce.Console.Extra/eCommerceContext.cs
+++ eCommerce.Console.Extra/eCommerceContext.cs
@@ -8,7 +8,7 @@ public class eCommerceContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder
-            .UseSqlServer("Server=localhost,1433;Database=ecommerce;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False;")
+            .UseSqlServer("Server=localhost,1433;Database=eCommerceTemp;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False;")
             .LogTo(System.Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
             .EnableSensitiveDataLogging();
     }
@@ -20,6 +20,14 @@ public class eCommerceContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Usuario>()
+            .ToTable("Usuarios", t => t.IsTemporal(b =>
+            {
+                b.HasPeriodStart("PeriodoInicial"); // Muda nome da coluna
+                b.HasPeriodEnd("PeriodoFinal"); // Muda nome da coluna
+                b.UseHistoryTable("UsuariosHistorico"); // Cria tabela para guardar informações
+            }));
+
         modelBuilder.Entity<Usuario>()
             .HasQueryFilter(a => a.SituacaoCadastro == SituacaoCadastro.Ativo);
 
